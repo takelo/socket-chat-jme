290b10175cfa28646fec80b27e4db3b30f3e0c46
Juan Manuel Echeverria <factorzero@gmail.com>
2022-02-18 13:57:29 -0300

    mensaje general por consola

---
+++ server/sockets/socket.js
@@ -0,0 +1,49 @@
+const { io } = require('../server');
+const { Usuarios } = require('../classes/usuarios');
+const { crearMensaje } = require('../utilidades/utilidades');
+
+const usuarios = new Usuarios();
+
+io.on('connection', (client) => {
+
+  client.on('entrarChat', (data, callback) => {
+
+    if( !data.nombre ){
+        return callback({
+            error: true,
+            mensaje: 'El nombre es necesario'
+        })
+    }
+    let personas = usuarios.agregarPersona( client.id, data.nombre );
+
+    client.broadcast.emit( 'listaPersonas', usuarios.getPersonas() );
+
+    callback( personas );
+    
+  });
+
+  client.on('crearMensaje', (data) => {
+
+    let persona = usuarios.getPersona(client.id);
+
+      let mensaje = crearMensaje( persona.nombre, data.mensaje);
+      client.broadcast.emit( 'crearMensaje' , mensaje );
+  })
+
+  client.on('disconnect', () => {
+
+    let personaBorrada = usuarios.borrarPersona(client.id);
+
+    // client.broadcast.to(personaBorrada.sala).emit('listaPersona', usuarios.getPersonasPorSala(personaBorrada.sala));
+    // client.broadcast.to(personaBorrada.sala).emit('crearMensaje', crearMensaje('Administrador', `${ personaBorrada.nombre } salió`));
+    client.broadcast.emit('crearMensaje', crearMensaje('Administrador', ` ${ personaBorrada.nombre } salió`));
+    client.broadcast.emit( 'listaPersonas', usuarios.getPersonas() );
+
+
+});
+
+
+
+
+
+});
